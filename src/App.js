9ec8523b98451b712d9c948b79b0c1cbbf57cd2a
김태영 <gimtaeyeong@gimtaeyeong-ui-MacBook-Pro.local>
2018-12-20 14:29:55 +0900

    delete Test page in Test.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                Start
+                Main 화면
             </div>
         );
     }
--- src/routers/Router.js
+++ src/routers/Router.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import App from '../App';
-import Test from '../pages/Test';
 
 class Router extends Component {
     render() {
@@ -9,7 +8,6 @@ class Router extends Component {
             <BrowserRouter>
                 <div>
                     <Route exact path="/" component={App} />
-                    <Route path="/Test" component={Test} />
                 </div>
             </BrowserRouter>
         );
