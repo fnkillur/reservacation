d9f58f7188fe534b1e23e8c7fb6508e628f4404b
hyunmin.shin <hyunmin.shin@rooffunding.com>
2019-01-18 18:05:59 +0900

    add fontawesome in modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './Modal.scss';
+import * as fontawesome from "@fortawesome/fontawesome-svg-core";
+import { faTimes } from '@fortawesome/fontawesome-free-solid'
+
+fontawesome.library.add(faTimes);
 
 class Modal extends Component {
     render() {
@@ -12,7 +17,7 @@ class Modal extends Component {
             <div className='backdrop'>
                 <div className='modal'>
                     <header>
-                        <button onClick={this.props.onClose}>X</button>
+                        <FontAwesomeIcon onClick={this.props.onClose} icon={faTimes}/>
                     </header>
                     {this.props.children}
                 </div>
